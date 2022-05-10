dcb6e11ccb5937c483dd44ded975ce1da812c254
Fredrick-Waihenya <98146814+Fredrick-Waihenya@users.noreply.github.com>
2022-05-10 16:17:35 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import BirthdayList from "./component/BirthdayList";
+import { birthdays, date } from "./birthdays.js";
+function App() {
+  const [Birthdays, setBirthdays] = useState([]);
+
+  useEffect(() => {
+    setBirthdays(birthdays);
+  }, [birthdays]);
+
+  const clearBirthdays = () => {
+    setBirthdays([]);
+  };
+  return (
+    <div className="App">
+      <div className="header__container">
+        <h2>Birthday Reminder 🍰</h2>
+      </div>
+      <div className="container">
+        <div className="date_counts">
+          <h3>
+            {date} - {birthdays.length} Birthdays
+          </h3>
+        </div>
+        <BirthdayList birthdays={Birthdays} />
+        <button onClick={clearBirthdays}>Clear All Birthdays</button>
+      </div>
+      <div className="madeby__container">
+        <p>Made with 💓 by Fredrick Waihenya on Jan - 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
